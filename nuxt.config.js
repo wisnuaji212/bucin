b6980ab3909a01bbe848488ec81330efc15ea9b8
wisnuaji212 <wisnuaji212@github.com>
2021-02-18 21:49:34 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
   head: {
     // titleTemplate: "%s - " + process.env.npm_package_name,
     // title: process.env.npm_package_name || "",
-    title: "I love you.",
+    title: "Halo Tiyas",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" },
