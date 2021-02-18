2f0404b8365ed8a7f4ca63133c06d57be2525b5d
wisnuaji212 <wisnuaji212@github.com>
2021-02-18 21:49:28 +0700

    Update stories.js

--- assets/stories.js
+++ assets/stories.js
@@ -1,6 +1,14 @@
 export default [
-  "Teks pertama",
-  "Teks kedua",
-  "Teks ke - n",
-  "Aku sayang kamu!"
+  "Halo Tiyas",
+  "Maaf kalo ini ga modal",
+  "Maaf juga kalo ini ga ada apa-apanya",
+  "Aku cuma mau bilang",
+  "Kayaknya lebih baik...",
+  "Kalo kita harus...",
+  "Celebrating 18th date on calendar at every month",
+  "Selamat tanggal 18 ya Ayang",
+  "Semoga langgeng terus",
+  "Semoga kedepannya kita bisa lebih baik ya",
+  "Stay focused on on our goals", 
+  "Aku sayang kamu Nur Wahyuning Tiyas!"
 ];
